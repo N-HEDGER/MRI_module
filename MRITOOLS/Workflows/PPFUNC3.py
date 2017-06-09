5127278ccc136f3696c0745632ecbbb01446f15a
N-HEDGER <nhedger1@gmail.com>
2017-06-09 14:22:42 +0100

    Annotate workflow

--- MRITOOLS/Workflows/PPFUNC3.py
+++ MRITOOLS/Workflows/PPFUNC3.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 13 16:33:40 2017
+Created on Fri Jun 9 12:00:00 2017
 @author: naah1g08
 """
 #--- Goal of the function:
-# Pre-processing pipeline.
 # Use dcm2nii, FSL and AFNI to form a funcitonal pre-processing pipeline
-# Inputs: directory of DICOM files, fwhm of Gaussian (smoothing), highpass filter (secs), TR
+
+# Inputs:
+# directory of DICOM files
+# fwhm of Gaussian smoother (mm)
+# highpass filter cutoff (secs).
+# TR (secs)
 
 # Outputs:
-# 1) / CONVERTED: nifti file.
-# 2) / REORIENTED reoriented nifti file.
-# 3) / CROPPED reoriented and cropped nifti file.
+# 1) / CONVERTED - nifti file (dcm2nii)
+# 2) / REORIENTED - reoriented nifti file (FSL).
+# 3) / SLICETIMED - Sicetimed file (FSL)
+# 4) / IMG2FLOAT - conversion to float (FSL)
+# 5) / MCORRECTED - motion corrected file (FSL)
+# 6) / DESPIKED - despiked file (AFNI)
+# 7) / MOTION PLOTTED - (FSL, actually contained within MCORRECTED)
+# 8) / EXTRACTED - extracted brain, extracted brain mask (FSL)
+# 9) / SMOOTHED - Smoothed data (AFNI)
+# 10) / HIGHPASSED - Temporal filtered data (FSL)
+# 11) / PREPROCESSED - Filtered data with mean added on (FSL)
 
-# Requires: dcm2nii, nipype, nilearn, nibabel. dcm2nii and fsl
+# MEANFUNCTIONAL - mean functional volume (FSL)
+# SHOWEXTRACT - visualise extraction (nilearn)
+# SHOWSMOOTH - visualise smoothing (nilearn)
+
+# Requires: dcm2nii, nipype, nilearn, nibabel, dcm2nii, fsl, afni
 
 
 #--- Details
 
-# dcm2nii is recruited for conversion, but fsl is recruited for cropping and reorienting
-# This is because dcm2nii doesnt seem to handle reorienting and cropping for partial brain 
-# functional data.
-# Reoriented files are plotted.
-# The cropping stage may not be useful for functional data, since I dont know how it affects slice timing.
+# Interleaved aquisition is assumed.
+# Slice timing is relative to middle slice
+# Motion correction is relative to middle volume, spline interpolation.
+# Extraction is plotted
+# Smoothing is plotted
 
 #--- 
 
@@ -41,7 +57,6 @@ import nipype.interfaces.io as nio           # Data i/o
 import nipype.interfaces.fsl as fsl          # fsl
 import nipype.interfaces.utility as util     # utility
 import nipype.pipeline.engine as pe          # pypeline engine
-import nipype.algorithms.rapidart as ra      # artifact detection
 import nipype.interfaces.fsl.utils as fslu
 import nipype.interfaces.fsl.preprocess as fslp
 from nipype.interfaces import afni as afni
@@ -49,89 +64,87 @@ from nipype.interfaces.utility import Function
 import matplotlib
 from nilearn import plotting
 from nilearn import image
-
 import os
 import nipype.interfaces.fsl as fsl          # fsl
 import nipype.interfaces.utility as util     # utility
 import nipype.pipeline.engine as pe          # pypeline engine
 import nipype.interfaces.freesurfer as fs    # freesurfer
-import nipype.interfaces.spm as spm
+
 
 tolist = lambda x: [x]
 highpass_operand = lambda x:'-bptf %.10f -1'%x
 
+#--- 2) Prompt user for directory containing DICOM FILES
 
 INITDIR=os.getcwd();
 
-workflow = pe.Workflow(name='FUNCPIPE')
-
-# Define inputs
-inputnode = pe.Node(interface=util.IdentityInterface(fields=['fwhm','highpass']),name='inputspec')
-
-
-#--- 3) Prompt user for directory containing DICOM FILES
+#--- 3) Prompt user for inputs.
 
 DICOMDIR=raw_input('Please drag in the directory of\nDICOM files you wish to convert\n(ensure there is no blank space at the end)')
-
-
-
-# Define inputs
 FWHM=input('Please enter the FWHM of the smoother')
+HIGHPASS=input('Please enter the High Pass filter cutoff')
+TR=input('Please enter the TR')
+
+
+#--- 4) Define workflow and input node.
+
+workflow = pe.Workflow(name='FUNCPIPE')
+inputnode = pe.Node(interface=util.IdentityInterface(fields=['fwhm','highpass','TR']),name='inputspec')
 
 inputnode.inputs.fwhm=FWHM
-inputnode.inputs.highpass=float(18)
+inputnode.inputs.TR=TR
+inputnode.inputs.highpass=float(HIGHPASS/(inputnode.inputs.TR*2.5))
 
 
-#--- 4) Move to directory
-
+#--- 5) Move to directory
 os.chdir(DICOMDIR)
 
 
-#--- 5) Set up converter node for conversion to nifti
+#--- 6) Set up converter node for conversion to nifti
 converter=pe.Node(interface=dcm2nii.Dcm2nii(),name='CONVERTED')
 converter.inputs.source_dir=DICOMDIR
 converter.inputs.gzip_output=bool(1)
 
 
-#--- 6) Set up realigner node to match orientation of MNI 152
+#--- 7) Set up realigner node to match orientation of MNI 152
 realigner=pe.Node(interface=fslu.Reorient2Std(),name='REORIENTED')
 realigner.inputs.output_type='NIFTI_GZ'
 
 workflow.connect(converter,'converted_files',realigner,'in_file')
 
 
-#--- 7) Set up a slice timing node
+#--- 8) Set up a slice timing node
 slicetimer=pe.Node(interface=fslp.SliceTimer(),name='SLICETIMED')
 slicetimer.inputs.interleaved = True
-slicetimer.inputs.time_repetition = float(2)
 
+workflow.connect(inputnode, 'TR', slicetimer, 'time_repetition')
 workflow.connect(realigner, 'out_file', slicetimer, 'in_file')
 
 
-#--- 8) Convert to float.
+#--- 9) Convert to float.
 img2float = pe.Node(interface=fsl.ImageMaths(out_data_type='float',op_string='',suffix='_dtype'),name='IMG2FLOATED')
 
 workflow.connect(slicetimer,'slice_time_corrected_file',img2float,'in_file')
 
-#--- 9) Motion correct.
+#--- 10) Motion correct.
 motion_correct = pe.Node(interface=fsl.MCFLIRT(save_mats=True,save_plots=True,interpolation='spline'),name='MCORRECTED')
 
 workflow.connect(img2float, 'out_file', motion_correct, 'in_file')
 
 
-#--- 10) Despike
+#--- 11) Despike
 despiker=pe.Node(interface=afni.Despike(),name='DESPIKED')
 despiker.inputs.outputtype = 'NIFTI_GZ'
 
 workflow.connect(motion_correct,'out_file',despiker,'in_file')
 
-#--- 11) Plot motion.
+#--- 12) Plot motion.
 plot_motion = pe.Node(interface=fsl.PlotMotionParams(in_source='fsl'),name='MOTIONPLOTTED')
 plot_motion.iterables = ('plot_type', ['rotations', 'translations'])
 
 workflow.connect(motion_correct, 'par_file', plot_motion, 'in_file')
 
-#--- 12) Extract
+#--- 13) Extract
 extracter=pe.Node(interface=fsl.BET(),name='EXTRACTED')
 extracter.inputs.frac=float(0.6)
 extracter.inputs.vertical_gradient=float(-0.1)
@@ -141,7 +154,7 @@ extracter.inputs.functional=bool(1)
 workflow.connect(despiker, 'out_file', extracter, 'in_file')
 
 
-#--- 11) Smooth
+#--- 14) Smooth
 smoother=pe.MapNode(interface=afni.BlurInMask(),name='SMOOTHED',iterfield=['fwhm'])
 smoother.inputs.outputtype='NIFTI_GZ'
 
@@ -150,20 +163,21 @@ workflow.connect(extracter, 'out_file', smoother, 'in_file')
 workflow.connect(extracter, 'mask_file', smoother, 'mask')
 
 
-#--- 11) Highpass filter
+#--- 15) Highpass filter
 
 # Filtering node
 highpass = pe.MapNode(interface=fsl.ImageMaths(suffix='_tempfilt'),name='HIGHPASSED',iterfield=['in_file'])
 
-
 workflow.connect(inputnode, ('highpass', highpass_operand), highpass, 'op_string')
 workflow.connect(smoother, 'out_file', highpass, 'in_file')
 
+#--- 16) Mean functional volume
 # Need to add back the mean removed by FSL
 meanfunc = pe.MapNode(interface=fsl.ImageMaths(op_string='-Tmean',suffix='_mean'),name='meanfunc',iterfield=['in_file'])
 workflow.connect(smoother, 'out_file', meanfunc, 'in_file')
 
-addmean = pe.MapNode(interface=fsl.BinaryMaths(operation='add'),name='ADDMEAN',iterfield=['in_file','operand_file'])
+#--- 17) Add mean back to highpassed data (FINAL OUTPUT)
+addmean = pe.MapNode(interface=fsl.BinaryMaths(operation='add'),name='PREPROCESSED',iterfield=['in_file','operand_file'])
 
 workflow.connect(highpass, 'out_file', addmean, 'in_file')
 workflow.connect(meanfunc, 'out_file', addmean, 'operand_file')
@@ -173,6 +187,8 @@ outputnode = pe.Node(interface=util.IdentityInterface(fields=['highpassed_files'
 workflow.connect(addmean, 'out_file', outputnode, 'highpassed_files')
 
 
+
+# Utility function for plotting extraction
 def bplot(in_file,in_file2,in_file3):
 	from nilearn import image
 	from nilearn import plotting
@@ -184,30 +200,45 @@ def bplot(in_file,in_file2,in_file3):
 	display.add_edges(in_file3)
 	return niftifiledim
 
-#--- 12) Show extract
-
+#--- 18) Show extraction
 showextract= pe.Node(Function(input_names=['in_file','in_file2','in_file3'],output_names=['niftifiledim'],function=bplot),name='SHOWEXTRACT')
 
 workflow.connect(despiker,'out_file', showextract,'in_file2')
 workflow.connect(extracter,'out_file', showextract,'in_file')
 workflow.connect(extracter,'mask_file', showextract,'in_file3')
 
+# Utility function for plotting extraction
+def splot(in_file):
+	from nilearn import image
+	from nilearn import plotting
+	niftifiledim=len(image.load_img(in_file).shape)
+	firstim=image.index_img(in_file, 0)
+	display=plotting.plot_anat(firstim, title = "Smoothed data",display_mode='z',cut_coords=10)	
+	return niftifiledim
 
-meanfunc2 = pe.Node(interface=fsl.ImageMaths(op_string='-Tmean',suffix='_mean'),name='meanfunc2')
+#--- 19) Show smoothing
+showsmooth= pe.MapNode(Function(input_names=['in_file'],output_names=['niftifiledim'],function=splot),iterfield=['in_file'],name='SHOWSMOOTH')
+
+workflow.connect(smoother,'out_file', showsmooth,'in_file')
+
+
+
+#--- 20) Mean functional volume (for plotting stats)
+meanfunc2 = pe.Node(interface=fsl.ImageMaths(op_string='-Tmean',suffix='_mean'),name='MEANFUNCTIONAL')
 workflow.connect(extracter, 'out_file', meanfunc2, 'in_file')
 workflow.connect(meanfunc2, 'out_file', outputnode, 'mean_functional_volume')
 
-
+#--- 21) Plot workflow
 workflow.base_dir = DICOMDIR
 workflow.write_graph(graph2use='exec')
 
-#--- 13) Run workflow
+#--- 22) Plot workflow
 result=workflow.run()
 
-#--- 14) Show plot
+#--- 23) Show plots
 matplotlib.pyplot.show()
 
-
+#--- 24) Return to initial working directory
 os.chdir(INITDIR)
 
 
