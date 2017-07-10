266ab0683ce45f0c1d76d61b0884991cf66520d4
N-HEDGER <nhedger1@gmail.com>
2017-07-10 14:23:33 +0100

    Update __init__.py

--- MRITOOLS/Nodes/__init__.py
+++ MRITOOLS/Nodes/__init__.py
@@ -3,7 +3,6 @@ print 'loading nodes'
 from .CONVERT import CONVERTER
 from .EXTRACT import EXTRACTER
 from .EXTRACT import VERBOSE_EXTRACTER
-from .REGIST import REGISTER
 from .SLICETIME import SLICETIMER
 from .MCORRECT import MCORRECTOR
 from .SMOOTH import SMOOTHER
