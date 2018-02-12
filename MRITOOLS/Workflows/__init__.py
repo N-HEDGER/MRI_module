af8e51a737c9eebdce82cdca5faf30541f3c9cf6
N-HEDGER <nhedger1@gmail.com>
2018-02-12 13:34:56 +0000

    Update __init__.py

--- MRITOOLS/Workflows/__init__.py
+++ MRITOOLS/Workflows/__init__.py
@@ -2,6 +2,7 @@ print 'loading workflows'
 
 from .FUNCWORK import FUNCPIPE
 from .NORMWORK import NORMPIPE
+from .NORMWORK import NORMPIPENEW
 from .RENDERWORK import RENDERPIPE
 from .L1WORK import L1PIPE
 from .L1WORK import FIRPIPE
